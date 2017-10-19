a2a4355854bfa860c69c20ed06a2cc119db614b2
JanineNing <18934230129@163.com>
2017-10-19 16:57:02 +0800

    lecture notes2

---
+++ git 的操作 gitbash.py
@@ -0,0 +1,60 @@
+
+UIC@CZ179413 MINGW64 ~
+$ cd ~/desktop
+
+UIC@CZ179413 MINGW64 ~/desktop
+$ git clone https://github.com/JanineNing/python1
+Cloning into 'python1'...
+warning: You appear to have cloned an empty repository.
+
+UIC@CZ179413 MINGW64 ~/desktop
+$ cd ~/python1
+bash: cd: /c/Users/UIC/python1: No such file or directory
+
+UIC@CZ179413 MINGW64 ~/desktop
+$ cd python/
+bash: cd: python/: No such file or directory
+
+UIC@CZ179413 MINGW64 ~/desktop
+$ cd python1/
+
+UIC@CZ179413 MINGW64 ~/desktop/python1 (master)
+$ git add .
+warning: LF will be replaced by CRLF in Chapter2 - Control Flow/.ipynb_checkpoints/Chapter2-Control Flow-checkpoint.ipynb.
+The file will have its original line endings in your working directory.
+warning: LF will be replaced by CRLF in Chapter2 - Control Flow/Chapter2-Control Flow.ipynb.
+The file will have its original line endings in your working directory.
+warning: LF will be replaced by CRLF in Chapter4 - Modules/.ipynb_checkpoints/Chapter4-Modules-checkpoint.ipynb.
+The file will have its original line endings in your working directory.
+warning: LF will be replaced by CRLF in Chapter4 - Modules/Chapter4-Modules.ipynb.
+The file will have its original line endings in your working directory.
+
+UIC@CZ179413 MINGW64 ~/desktop/python1 (master)
+$ git commit -m 'lecture notes'
+[master (root-commit) 12b0fa9] lecture notes
+ 10 files changed, 2166 insertions(+)
+ create mode 100644 Chapter2 - Control Flow/.ipynb_checkpoints/Chapter2-Control Flow-checkpoint.ipynb
+ create mode 100644 Chapter2 - Control Flow/Chapter2-Control Flow.ipynb
+ create mode 100644 Chapter2 - Control Flow/Sequential.png
+ create mode 100644 Chapter2 - Control Flow/if.png
+ create mode 100644 Chapter2 - Control Flow/number-triangle.png
+ create mode 100644 Chapter2 - Control Flow/while-loop.png
+ create mode 100644 Chapter4 - Modules/.ipynb_checkpoints/Chapter4-Modules-checkpoint.ipynb
+ create mode 100644 Chapter4 - Modules/Chapter4-Modules.ipynb
+ create mode 100644 Chapter4 - Modules/create-your-own-module.png
+ create mode 100644 Chapter4 - Modules/package.png
+
+UIC@CZ179413 MINGW64 ~/desktop/python1 (master)
+$ git push origin master
+Counting objects: 14, done.
+Delta compression using up to 8 threads.
+Compressing objects: 100% (14/14), done.
+Writing objects: 100% (14/14), 1.02 MiB | 136.00 KiB/s, done.
+Total 14 (delta 0), reused 0 (delta 0)
+To https://github.com/JanineNing/python1
+ * [new branch]      master -> master
+
+UIC@CZ179413 MINGW64 ~/desktop/python1 (master)
+$
+
+
